3e32e39937c0a366dcb6554d0f5369cf59ef505b
naeglinghaff <kate.o.haffenden@gmail.com>
2020-05-15 14:47:36 +0100

    Added graph render to front end

--- public/chartMapper.js
+++ public/chartMapper.js
@@ -6,19 +6,21 @@ class ChartMapper {
   }
 
   map(data){
+    let res = data.timer
     const xlabels = [];
     const values = [];
     const ctx = document.getElementById('chart').getContext('2d');
-    for(var i = 0; i < data.length; i++){
-      xlabels.push = data[i][0];
-      values.push = data[i][1];
+    for(var i = 0; i < res.length; i++){
+      xlabels.push(res[i][0]);
+      values.push(res[i][1]);
     }
     const myLineChart = new Chart(ctx, {
       type: 'line',
       data: {
-        labels: [ xlabels ],
+        labels: xlabels,
         datasets: [{
           label: 'Time Performance of Algorithm',
+          borderColor: 'rgb(76, 166, 134)',
           data: values
         }]
       },
