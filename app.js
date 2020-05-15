6d679850794fa121dd2b1be869112628162b9a23
naeglinghaff <kate.o.haffenden@gmail.com>
2020-05-15 10:18:54 +0100

    Removed redundant method options on base.html, added timer data in the response

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let AlgorithmTimer = require("./src/algorithm_timer");
+let timer = new AlgorithmTimer;
 let app = express();
 app.use(express.static('public'));
 app.use(express.json());
@@ -9,8 +10,15 @@ app.get("/", (req, res) => {
 });
 
 app.post('/api', (req, res) => {
-  console.log(req.body);
-  res.end();
+  nameditem = req.body.value;
+  console.log(nameditem);
+  console.log(String(nameditem));
+  let data = timer.recordTime(String(nameditem));
+  console.log(data);
+  res.json({
+    status: 'success',
+    timer: data
+  });
 })
 
 const port = 3000;
