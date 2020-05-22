293c2d35f052d8acffabef28be47d9ffb76f4784
naeglinghaff <kate.o.haffenden@gmail.com>
2020-05-22 14:13:49 +0100

    Added additional tests to algorithm timer

--- __tests__/algorithm.timer.test.js
+++ __tests__/algorithm.timer.test.js
@@ -7,11 +7,27 @@ describe('Algorithm Timer', () => {
     timer = new AlgorithmTimer;
   })
 
-  describe('record time of sort function', () => {
-    it('returns a range of values, containing the sizes of the arrays', () => {
+  describe('recordTime - sort', () => {
+    it('returns a range of array lengths when sort is given as param', () => {
       let result = timer.recordTime('sort')
       expect(result[0]).toContain(5000);
       expect(result[19]).toContain(100000);
+    })
+    it('does not exceed 100,000 as the maximum length of the array', () => {
+      let result = timer.recordTime('sort')
+      expect(result).not.toContain(100000);
+    })
+  })
+
+describe('recordTime - reverse', () => {
+    it('returns a range of array lengths when reverse is given as a param', () => {
+      let result = timer.recordTime('reverse')
+      expect(result[0]).toContain(5000);
+      expect(result[19]).toContain(100000);
+    })
+    it('does not exceed 100,000 as the maximum length of the array', () => {
+      let result = timer.recordTime('reverse')
+      expect(result).not.toContain(100000);
     })
   })
 })
