63adef3df281650e3c8a2d242f2f63222885b4e1
naeglinghaff <kate.o.haffenden@gmail.com>
2020-07-01 10:18:47 +0100

    Correct variable name in tests

--- __tests__/custom_sort_test.js
+++ __tests__/custom_sort_test.js
@@ -1,15 +1,15 @@
 describe('Custom Sort', () => {
 
-  let dep = require('../src/custom_algorithms');
+  let dup = require('../src/custom_algorithms');
   let array;
 
   it('returns a sorted array', () => {
       array = [10, 5, 7, 4, 2];
-      expect(dep.customSelectionSort(array)).toEqual([2, 4, 5, 7, 10]);
+      expect(dup.customSelectionSort(array)).toEqual([2, 4, 5, 7, 10]);
     })
 
   it('returns a sorted array', () => {
       array = [10, 5, 7, 2, 4, 2];
-      expect(dep.customSelectionSort(array)).toEqual([2, 2, 4, 5, 7, 10]);
+      expect(dup.customSelectionSort(array)).toEqual([2, 2, 4, 5, 7, 10]);
     })
 })
